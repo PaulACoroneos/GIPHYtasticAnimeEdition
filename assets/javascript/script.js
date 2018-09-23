5c428a24e9580a8b9b999940ccf82e2a17a2c19b
Paul Coroneos <paulalexandercoroneos@gmail.com>
2018-09-23 17:47:44 -0500

    minimum requirements complete. Working on bonus tasks

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,8 @@
 var objImage;
 var topics = ["naruto","kurosaki","inuyasha","kinomoto","yugioh","luffy","Midoriya"];
+var global_response;
 
-//generates a button either at app start or dynamically from text field
+//function generates a button and appends it to #button-area
 function buttonGen(object) {
     var button = $("<button>");
     button.attr("value",object);
@@ -10,19 +11,43 @@ function buttonGen(object) {
     $(".button-area").append(button);
 }
 
-//okay lets render initial buttons
-for(var i =0; i<topics.length;i++) {
-    buttonGen(topics[i]); //button generate
-}
-
-//When text field is submitted generate a button! (using built in bootstrap to throw error if field empty)
+//When text field is submitted generate a button! (using built in html5 to prevent submission if field empty)
 $(".object-button").on("click", function(e) {
     e.preventDefault(); //DON'T REFRESH PAGE
     var object = $("#button-gen").val();    //get value in text field 
+    topics.push(object); //add button to topics array
+    console.log(topics);    
     if(object.length >0) {  //is it not empty
         buttonGen(object);
         $("#button-gen").val("");
     }
+
+});
+
+//when card-body is clicked in image-area animate it if it's not animated. Otherwise don't animate it
+$(".image-area").on("click",".card", function(e) {
+
+    var temp = $(this).find('.card-img-top').attr("src");
+    console.log("image src on click: "+temp);  
+
+    //let's see if we are displaying animated or still version
+    if(temp.includes("_s.gif") ) {   //does img src contain _s.gif
+
+        //replace img src with animated
+        console.log("still detected");
+        temp = temp.replace("_s.gif", ".gif"); //replace still delimiter with normal delimiter for animated version
+        console.log(temp);
+        $(this).find('.card-img-top').attr("src",temp);
+
+    }
+    else {
+        //replace img src with still version
+        console.log("animated detected");
+         temp =temp.replace(".gif", "_s.gif"); //replace still delimiter with normal delimiter for animated version
+        console.log(temp);
+        $(this).find('.card-img-top').attr("src",temp);    
+    }
+
 });
 
 //when generated object button is pressed
@@ -33,7 +58,7 @@ $(".button-area").on("click",".objecty", function(e) {
     // create URL for get request + field input
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=Z4v8bNQShUGV21HziHzyKa5Xx8fVH8sX&q="+objectName+"&limit=150";
     console.log(queryURL);
-    // call to API using stored URL. Only call the API once!
+    // call to API using stored URL. Only call the API once per feedback of my TA's
     
     $.ajax({
     url: queryURL,
@@ -44,12 +69,13 @@ $(".button-area").on("click",".objecty", function(e) {
     .then(function(response) {
         console.log(response);
 
+        global_response = response; //need to store this globally for animated/non-animated
         for(var i=0; i<10;i++) {
 
             //rando = Math.floor(Math.random()*99)+1; //generate random result from GET request
 
             // store URL of cat image into a var
-            var imageUrl = response.data[i].images.original.url;    //location of gif in giphy
+            var imageUrl = response.data[i].images.original_still.url;    //location of gif in giphy with still version
             var imgRating =response.data[i].rating;  //grab rating from JSON 
             var imgTitle = response.data[i].title;
 
@@ -67,30 +93,36 @@ $(".button-area").on("click",".objecty", function(e) {
             var cardList = $("<ul>");
             cardList.addClass("list-group list-group-flush");
 
+            //create list element for GIF rating
             var rating = $("<li>");
-            rating.attr("text",imgRating);
+            rating.text("Rating: "+imgRating);
             rating.addClass("list-group-item");
 
+            //create list element for GIF title
             var title = $("<li>");
-            title.attr("text",imgTitle);
+            title.text("GIF Title: "+imgTitle);
             title.addClass("list-group-item");
-            
+
             cardList.append(rating);
             cardList.append(title);
 
             var cardBody = $("<div>");
             cardBody.addClass("card-body");
 
-            //put together card
+            //put together final card
             cardBody.append(cardList);
             card.append(img);
             card.append(cardBody);
 
-
-            // add id images to catImage img element
+            // add final card to GIF image area
             $(".image-area").prepend(card);
         }
     });
 
 
 });
+
+//initialize the trivia list buttons
+for(var i =0; i<topics.length;i++) {
+    buttonGen(topics[i]); //button generate
+}
