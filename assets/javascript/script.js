cf716f4df30fa25bc2fd6596cc68b3715d36cf24
Paul Coroneos <paulalexandercoroneos@gmail.com>
2018-09-25 22:53:57 -0500

    correct bug in favorites. still working on local storage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -50,30 +50,58 @@ $(".image-area").on("click",".card-img-top", function(e) {
 
 });
 
+$(".favorite-area").on("click",".card-img-top", function(e) {
+
+    var temp = $(this).attr("src");
+    console.log("image src on click: "+temp);  
+
+    //let's see if we are displaying animated or still version
+    if(temp.includes("_s.gif") ) {   //does img src contain _s.gif
+
+        //replace img src with animated
+        console.log("still detected");
+        temp = temp.replace("_s.gif", ".gif"); //replace still delimiter with normal delimiter for animated version
+        console.log(temp);
+        $(this).attr("src",temp);
+
+    }
+    else {
+        //replace img src with still version
+        console.log("animated detected");
+        temp =temp.replace(".gif", "_s.gif"); //replace still delimiter with normal delimiter for animated version
+        console.log(temp);
+        $(this).attr("src",temp);    
+    }
+
+});
+
 //if favorite button is pressed add the card corresponding to the favorite button to the favorite area
 $(".image-area").on("click",".card .favorite", function(e) {
     e.preventDefault();
     var favCard = $("<div>");
-    favCard.addClass("card mb-1");
+    favCard.addClass("card mb-1");  
     favCard.attr("style","width: 18rem");
-    console.log("favorite clicked");
     var temp = $(this).closest('.card').html();    //capture card info into var
-    console.log(temp);
     favCard.append(temp);
     $(".favorite-area").append(favCard);
+    stored.push(temp.innerHTML);  //add to favorites array
+    console.log(temp.innerHTML);
+    localStorage.setItem("favorites", JSON.stringify(stored));   //update keyword with new card added
+
 
 });
 
-// //if download button is pressed download a copy of the photo
-// $(".image-area").on("click",".card .download", function(e) {
-//     e.preventDefault();
-//     var favCard = $("<div>");
-//     favCard.addClass("card mb-1");
-//     favCard.attr("style","width: 18rem");
-//     console.log("download clicked");
-//     var temp = $(this).closest('.card-img-top').html();    //capture img data
+//if download button is pressed download a copy of the photo
+$(".image-area").on("click",".card .download", function(e) {
+    e.preventDefault();
+    //we are going to create an anchor tag and take advantage of download 
+    var favCard = $("<div>");
+    favCard.addClass("card mb-1");
+    favCard.attr("style","width: 18rem");
+    console.log("download clicked");
+    var temp = $(this).closest('.card-img-top').html();    //capture img data
 
-// });
+});
 
 //when generated object button is pressed
 $(".button-area").on("click",".objecty", function(e) {
@@ -159,7 +187,18 @@ $(".button-area").on("click",".objecty", function(e) {
 
 });
 
-//initialize the trivia list buttons
+//initialize the topics list buttons
 for(var i =0; i<topics.length;i++) {
     buttonGen(topics[i]); //button generate
+}
+
+//check localstorage for presence of favorites and import to webpage
+var stored = JSON.parse(localStorage.getItem("favorites"));
+if(stored) {
+    console.log(stored);
+    $(".favorite-area").push(stored);
+}
+else {
+    var stored = [];    //create empty array
+    localStorage.setItem("favorites", JSON.stringify(stored));
 }
